e9f147f2c0db83272e46a75ad1f299e13632269b
Jeanetted3v <jeanetted3v@gmail.com>
2022-01-05 19:05:59 +0800

    Initial Commit

---
+++ MerxBot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MerxBot
+//
+//  Created by Zhao Qianyu on 5/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MerxBot/MerxBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MerxBotApp.swift
+//  MerxBot
+//
+//  Created by Zhao Qianyu on 5/1/22.
+//
+
+import SwiftUI
+
+@main
+struct MerxBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
